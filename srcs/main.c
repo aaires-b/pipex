c3d2aeb5775a179477a7879092b293c4fef5611d
Ana Aires Belmonte <aaires-b@c2r6s1.42lisboa.com>
2024-03-02 22:47:38 +0000

    oi

--- srcs/main.c
+++ srcs/main.c
@@ -12,105 +12,69 @@
 
 #include "../includes/pipex.h"
 
-t_cmds *get_cmds()
+void erro(int errno)
 {
-	static t_cmds cmds;
-	return(&cmds);
+	strerror(errno);
+	exit(1);
 }
-
-void child_process(int *fd)
+void child_process(int *fd, char **av, char **env)
 {
-	t_cmds *cmds = get_cmds();
-
-	cmds->fd_file1 = open(cmds->file1, O_RDONLY, 0777);
-	if(cmds->fd_file1 == -1)
-	{
-		strerror(errno);
-		exit(1);
-	}
-	dup2(cmds->fd_file1, 0); //  the new file descriptor  is adjusted so that it now
-       						//refers to the same open file description as old fd.
-	dup2(fd[1], 1);
-	close(fd[0]);
-	execve(cmds->path_cmd1, &cmds->av[2], cmds->env);
-}
-void parent_process(int *fd)
-{
-	t_cmds *cmds = get_cmds();
-
-	cmds->fd_file2 = open(cmds->file2, O_CREAT | O_RDWR | O_TRUNC, 0644);
-	if(cmds->fd_file2 == -1)
-	{
-		strerror(errno);
-		exit(1);
-	}
-	dup2(fd[0], 0); //  the new file descriptor  is adjusted so that it now
+	int fd_file1;
+	
+	fd_file1 = open(av[1], O_RDONLY, 0777);
+	if(fd_file1 == -1)
+		erro(errno);
+	dup2(fd_file1, 0); //  the new file descriptor  is adjusted so that it now
 							//refers to the same open file description as old fd.
-	dup2(cmds->fd_file2, 1);
+	dup2(fd[1],1);
 	close(fd[1]);
-	execve(cmds->path_cmd2, &cmds->av[3], cmds->env);
+	close(fd[0]);
+	executer(av[2], env);
 	//printf("aqui\n");
 }
 
-void start()
+void child_process2(int *fd, char **av, char **env)
 {
-	// t_cmds *cmds = get_cmds();
-
-	// cmds->n1_stdin = open(cmds->file1, O_RDONLY, 007);
-	// cmds->n2_stdout = open(cmds->file2, O_CREATE)
-	int fd[2];
-	pid_t proc_id; // signed integer , represent a process ID.
+	int fd_file2;
 	
-	//Pipes provide a way for one process to send data to another process. 
-	//create a unidirectional communication channel. returns -1 for error fd[1] = writes , fd[0] = reads
-	//returns two file descriptors: one for the read end of the pipe and one for the write end.
-	if(pipe(fd) == -1)
-	{
-		strerror(errno);//perror : print an error message to stderr based on the error state stored in errno
-		//errno is a global variable indicating the error occurred during any function call
-		// strerror : returns a pointer to the textual representation of the current errno value.
-		exit(1);
-	}
-	// create new process(cpy of parent). The child process inherits the file descriptors of the parent process, including those of the pipe.
-	proc_id = fork();
-	if(proc_id == -1)
-	{
-		strerror(errno);
-		exit(1);
-	}
-	if(proc_id == 0) // im in the new process (child) 
-	{
-		child_process(fd);
-	}
-	waitpid(proc_id, NULL, 0);
-	parent_process(fd);
-	
- 	// If pid is equal to zero, waitpid() waits for termination of any child whose process group ID is equal to that of the caller.
-	// makes the parent process wait for the child process to be terminated by default
-	// The status_ptr pointer NULL =  waitpid() ignores the child's return status.
-	// options 0 = Specifies additional information for waitpid() , 0 means to block and wait until the specified child process terminates. 
-}
-
-void set_cmds(int ac,char **av, char **env)
-{
-	t_cmds *cmds = get_cmds();
-	cmds->ac = ac;
-	cmds->av = av;
-	cmds->cmd1_flag = 0;
-	cmds->cmd2_flag = 0;
-	cmds->env = env;
-	cmds->file1 = av[1];
-	cmds->file2 = av[4];
-	cmds->cmd1 = ft_strjoin("/", av[2]);
-	cmds->cmd2 = ft_strjoin("/", av[3]);
-	
+	fd_file2 = open(av[4], O_RDWR | O_CREAT | O_TRUNC, 0644);
+	if(fd_file2 == -1)
+		erro(errno);
+	dup2(fd[1], 0); //  the new file descriptor  is adjusted so that it now
+							//refers to the same open file description as old fd.
+	dup2(fd_file2, 1);
+	close(fd[1]);
+	close(fd[0]);
+	executer(av[3], env);
+	//printf("aqui\n");
 }
 
 int main(int ac,char **av, char **env)  // env = da-me as environmental variables do meu pc. 
 {
-	set_cmds(ac, av, env);
-	parse_cmds();
-	start();
+	if(ac == 5)
+	{
+		int fd[2];
+		int proc_id1;
 
+		if(pipe(fd) == -1)
+			erro(errno);
+		proc_id1 = fork();
+		if(proc_id1 == -1)
+			erro(errno);
+		if(proc_id1 == 0)
+			child_process(fd, av, env);
+		int proc_id2 = fork();
+		if(proc_id2 == -1)
+			erro(errno);
+		if(proc_id2 == 0)
+			child_process2(fd, av, env);
+		waitpid(proc_id1, NULL, 0);
+		waitpid(proc_id2, NULL, 0);
+	}
+	else
+	{
+		////mensagem de erro
+	 	exit(1);
+	}
 	return (0);
 }
--- srcs/parsing.c
+++ srcs/parsing.c
@@ -12,71 +12,53 @@
 
 #include "../includes/pipex.h"
 
-void parse_cmds()
+char  *path( char **env)
 {
-	int fd1;
-	t_cmds *cmds;
 	int i;
 	int k;
 
 	k = 0;
 	i = 0;
-	cmds = get_cmds();
-	//printf("ac : %d\n", cmds->ac);
-	//printf("av : %s\n", cmds->av[1]);
-	//printf("env : %s\n", cmds->env[1]);
-	if(cmds->ac != 5)
-		exit(1);
-	fd1 = open(cmds->file1, O_RDONLY, 0777);
-	// fd2 = open(cmds->file2, O_RDONLY);
-	if(fd1 == -1)
-		exit(1);
-	if(fd1 != -1)
-		close(fd1);
-	while(cmds->env[i])
+	char *path;
+	while(env[i])
 	{
-		if(ft_strncmp("PATH=", cmds->env[i], 4) == 0)
-			cmds->path = ft_split(&cmds->env[i][k + 5],':');
-		i++;
-	}
-	i = 0;
-	while(cmds->path[i] != NULL)
-	{
-		char *a = ft_strjoin(cmds->path[i], cmds->cmd1);
-		if(access(a, F_OK) == 0)
+		if(ft_strncmp("PATH=", env[i], 4) == 0)
 		{
-			cmds->path_cmd1 = a;
-			cmds->cmd1_flag = 1;
-			break ; 
+			path = ft_split(env[i][k + 5],':');
+			return(path);
 		}
-		free(a);
 		i++;
 	}
-	i = 0;
-	while(cmds->path[i] != NULL)
-	{
-		char *b = ft_strjoin(cmds->path[i], cmds->cmd2);
-		if(access(b, F_OK) == 0)
-		{
-			cmds->cmd2_flag = 1;
-			cmds->path_cmd2 = b;
-			break ;
-		}
-		free(b);
-		i++;
-	}
-	if(cmds->cmd1_flag == 0 || cmds->cmd2_flag == 0)
-	{
-		printf("erro\n");
-		exit(1);
-		//falta free dos paths
-	}
-
-
+	return (NULL);
 }
-// 	i = 0;
-// 	while(cmds->path[i])
-// 	{
-// 		printf(" env cortado : %s", cmds->path[i]);
-// 		i++;
-// 	}
+
+char *path2(char **path, char *cmd)
+{
+	int i = 0;
+	char *a;
+	char *path2;
+
+	i = 0;
+	a = ft_strjoin("/", cmd);
+	while(path[i] != NULL)
+	{
+		path2 = ft_strjoin(path[i], a);
+		if(access(path2, F_OK) == 0)
+		{
+			return(path2);
+		}
+		free(path2);
+		i++;
+	}
+	free(a);
+	return(NULL);
+}
+
+
+
+void executer(char *cmd, char **env)
+{
+	char *path;
+	path = path2(find_path(env), cmd);
+	execve(path, cmd, env);
+}
